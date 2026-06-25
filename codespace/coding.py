ffd65c042448768adb79500a9b671d9946ebec6a
夏 sam <138611923+samttsummer@users.noreply.github.com>
2026-06-25 18:46:03 -0300

    Update and rename coding to coding.py

---
+++ codespace/coding.py
@@ -0,0 +1,44 @@
+# addDestination and openRoutes
+
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+
+# Setup and initialization
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
+driver.implicitly_wait(2)
+
+driver.get("https://www.google.com/maps")  # Adjusted URL to match the context of Maps/Routes
+
+
+def add_destination(address):
+    empty_bar = driver.find_element(By.ID, 'searchboxinput')
+    empty_bar.clear()
+    empty_bar.send_keys(address)
+    empty_bar.send_keys(Keys.RETURN)
+
+
+def open_routes():
+    xpath = '//button[@data-value="Rotas"]'
+    wait = WebDriverWait(driver, timeout=5)
+    routes_button = wait.until(EC.presence_of_element_located((By.XPATH, xpath)))
+    routes_button.click()
+
+
+if __name__ == '__main__':
+    target_address = 'Rua Afonsina, 175 - Rudge Ramos, São Paulo - SP, 09633-000'
+    
+    # Executing the functions
+    add_destination(target_address)
+    
+    # Optional: If you want to open routes right after adding the destination
+    # open_routes()
+
+    time.sleep(600)
